72359aba254d94fc24a310e261cbe8b3773dc174
aldercj1@gmail.com <aldercj1@gmail.com>
2022-10-11 17:19:30 +0100

    add parse viral script

---
+++ scripts/parse_viral.py
@@ -0,0 +1,113 @@
+import argparse
+import pandas as pd
+import taxonomy
+
+
+# SNAKEMAKE ARGUMENTS
+NODES = snakemake.config["taxonomy"]["nodes"]
+NAMES = snakemake.config["taxonomy"]["names"]
+CENTRIFUGE_FILE = snakemake.input.raw
+
+# NODES = "ref/refseq/taxonomy/nodes.dmp"
+# NAMES = "ref/refseq/taxonomy/names.dmp"
+
+
+def get_species(query, tax, name=False):
+    if name:
+        taxID = tax.find_by_name(query).id
+    else:
+        taxID = str(query)
+    if taxID is None:
+        raise IOError("Invalid name")
+    if tax.node(taxID) is None:
+        return None
+    if tax.parent(taxID, at_rank="species group") is None:
+        organisms = tax.parent(taxID, at_rank="species").name
+    else:
+        organisms = tax.parent(taxID, at_rank="species group").name
+    return organisms
+
+def split_hits(df, tax):
+    """
+    Split centrifuge output into two categories: Unique hits and multi-match hits.
+    Multi-match hits are further checked to see if hits are to the same taxonomy id/rank
+    (ranks below species bought up to species)
+    Also filters out reads below threshold
+    Parameters
+    ----------
+    df
+        centrifuge output as pandas DataFrame object
+    tax
+        taxonomy object from module taxonomy
+    Returns
+    -------
+    unique_dict
+        dictionary in format;  {readID: taxID}
+    multi_dict
+        dictionary in format; {readID:[taxIDs]}
+    """
+
+    # Split hits
+    unique_df = df[df["numMatches"] == 1]
+    multi_df = df[df["numMatches"] != 1]
+
+    unique_dict = dict()
+    multi_dict = dict()
+
+    zipped = zip(unique_df["readID"], unique_df["taxID"])
+    for r, t in zipped:
+        unique_dict[r] = species_id(str(t), tax)
+
+    # Check if multiple hits for read belong to same taxID i.e. Are actually unique hit
+    multi_group = multi_df.groupby("readID")
+    for read, dat in multi_group:
+        tax_nodes = [species_id(str(t),tax) for t in dat["taxID"].unique() if species_id(str(t),tax) is not None]
+        tax_ids = set(t.id for t in tax_nodes)
+        if len(tax_ids) == 1:
+            unique_dict[read] = tax_nodes[0]
+        else:
+            multi_dict[read] = tax_nodes
+
+    # Bring taxIDS with ranks below species up to level
+
+    return unique_dict, multi_dict
+
+
+def lca(multi_dict, tax):
+    lca_dict = dict()
+    for k,v in multi_dict.items():
+        lca = v[0]
+        for i in range(1,len(v)):
+            lca = tax.lca(lca.id, v[i].id)
+        lca_dict[k] = v
+    return lca_dict
+
+     
+
+def main():
+    tax = taxonomy.Taxonomy.from_ncbi(NODES, NAMES)
+    df = pd.read_csv(CENTRIFUGE_FILE, sep="\t")
+    report_dict, multi_hit_dict = split_hits(df, tax)
+    lca_dict = lca(multi_hit_dict, tax)
+    report_dict.update(lca_dict)
+    read_df = pd.DataFrame.from_dict(report_dict, orient="index").reset_index()
+    read_df.columns = ["readID", "TaxID"]
+    read_df["TaxID"] = read_df["TaxID"].apply(lambda x: x.id if (x is not None) else "NA")
+    read_df["Organism"] = read_df["TaxID"].apply(lambda x: get_name(x,tax)) ## swap around
+    read_df.to_csv(READ_OUTPUT, index=False, sep="\t")
+
+    report_values = Counter([int(i.id) if i is not None else "No ID" for i in report_dict.values()])
+    del report_values[9606]
+    total_counts = sum(report_values.values())
+
+    #Report output
+    report_df = pd.DataFrame.from_dict(report_values, orient="index").reset_index()
+    report_df.columns = ["Tax_ID", "Counts"]
+    report_df["Organism"] = report_df["Tax_ID"].apply(lambda x: tax.node(str(x)).name if (tax.node(str(x)) is not None) else "ARGOS ISOLATE")
+    report_df["Percentage"] = report_df["Counts"] / total_counts * 100
+    report_df = report_df.sort_values(by="Percentage", ascending=False)
+    report_df = report_df[["Organism", "Tax_ID", "Counts", "Percentage"]]
+    report_df.to_csv(REPORT_OUTPUT, index=False, sep="\t")
+
+if __name__ == "__main__":
+    main()
